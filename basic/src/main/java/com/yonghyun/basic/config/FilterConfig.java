4f11e469be792f63c41a2970cff543ba5751d2cd
yonghyun Jeong <jayjay@kakao.com>
2019-12-24 17:02:01 +0900

    add filter interceptor class

---
+++ basic/src/main/java/com/yonghyun/basic/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.yonghyun.basic.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.yonghyun.basic.filter.IPCheckFilter;
+
+@Configuration
+public class FilterConfig {
+	@Bean
+	public FilterRegistrationBean<Filter> getFilterRegistrationBean() {
+		FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new IPCheckFilter());
+		bean.addUrlPatterns("/visitor");
+		return bean;
+	}
+}

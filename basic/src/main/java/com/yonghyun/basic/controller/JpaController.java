9a6474f2a5a7a0163dc86808036b2653501ed01e
yonghyun Jeong <jayjay@kakao.com>
2019-12-26 09:20:20 +0900

    Add jpa controller

---
+++ basic/src/main/java/com/yonghyun/basic/controller/JpaController.java
@@ -0,0 +1,29 @@
+package com.yonghyun.basic.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.yonghyun.basic.model.Product;
+import com.yonghyun.basic.repository.ProductRepository;
+
+
+@RestController
+public class JpaController {
+	@Autowired
+	ProductRepository productRepository;
+
+	@GetMapping("/jpa/product")
+	public List<Product> product() {
+		List<Product> list = productRepository.findAll();
+		return list;
+	}
+
+	@PostMapping("/jpa/product")
+	public String productPost(@ModelAttribute Product product) {
+		productRepository.save(product);
+		return "redirect:/jpa/product";
+	}
+}
